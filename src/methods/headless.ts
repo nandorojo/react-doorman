96c409f325e9c0c7638a8ee62c3e672faed8002f
Fernando Rojo <frojo@sas.upenn.edu>
2021-06-03 13:47:53 -0400

    fix headless func

--- src/methods/headless.ts
+++ src/methods/headless.ts
@@ -3,7 +3,7 @@ import type Firebase8 from 'firebase/app'
 export let onIdTokenChangedHeadless: ReturnType<
   typeof Firebase8['auth']
 >['onIdTokenChanged'] = (callback) => {
-  const firebase8: typeof Firebase8 = require('firebase/app')
+  const firebase8: typeof Firebase8 = require('firebase/app').default
   return firebase8.auth().onIdTokenChanged(callback)
 }
 
@@ -12,16 +12,14 @@ export let signInWithCustomTokenHeadless = (
     ReturnType<typeof Firebase8['auth']>['signInWithCustomToken']
   >[0]
 ) => {
-  require('firebase/auth')
-  const firebase8: typeof Firebase8 = require('firebase/app')
+  const firebase8: typeof Firebase8 = require('firebase/app').default
   return firebase8.auth().signInWithCustomToken(token)
 }
 
 export let signOutHeadless: ReturnType<
   typeof Firebase8['auth']
 >['signOut'] = () => {
-  require('firebase/auth')
-  const firebase8: typeof Firebase8 = require('firebase/app')
+  const firebase8: typeof Firebase8 = require('firebase/app').default
   return firebase8.auth().signOut()
 }
 
